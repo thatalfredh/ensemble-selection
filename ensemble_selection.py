9452a6d8238ede5a2ccaf9aa75f84edd268059e9
thatalfredh <bizcelearningresources@gmail.com>
2021-07-10 19:35:41 +0800

    Code Refactor

--- ensemble_selection.py
+++ ensemble_selection.py
@@ -1,128 +1,120 @@
-# ensemble usage
-from collections import Counter
+from collections import Counter # for update_weights
 
-def update_ensemble_weights(ensemble):
-    
-    """ This function computes the weights assigned to each model in the ensemble based on frequency """
-    
-    #from collections import Counter
-    
+import numpy as np # for ensemble prediction
+from keras.metrics import binary_crossentropy
+
+np.random.seed(42)
+
+def update_weights(ensemble):
+    """ 
+    Compute the weights assigned to 
+    each model in the ensemble based on frequency 
+    ensemble: {'models':[], 'weights':{}, 'predictions':{}} 
+    """
     occurrences = Counter(ensemble['models'])
-    nb_samples = sum([cnt for model_idx, cnt in occurrences.items()])
-    
-    for model_idx in ensemble['models']:
-        ensemble['weights'][model_idx] = occurrences[model_idx]/nb_samples
-        
-    return ensemble    
+    total = sum(occurences.values())
+    ensemble['weights'] = [occurrences[model_idx]/total for model_idx in ensemble['models']]
+                           
+    return ensemble  
 
-def compute_ensemble_loss(ensemble, y_val):
-    
-    """ This function computes the ensemble loss from weighted ensemble predictions """
-    
-    #import numpy as np
-    #from keras.metrics import binary_crossentropy
-    
-    predictions = 0
-    for model_idx in ensemble['models']:
-#         print(np.array(ensemble['predictions'][model_idx])) 
-#         print(ensemble['weights'][model_idx])
-        predictions += np.array(ensemble['predictions'][model_idx]) * ensemble['weights'][model_idx]
-    
-    ensemble_loss = binary_crossentropy(y_true=y_val, y_pred=predictions).numpy().mean()
+# TO DO: modify for multi-class
+def compute_loss(ensemble, y_val):
+    """ 
+    Compute the ensemble loss from weighted ensemble predictions (cached)
+    ensemble: <dict> {'models': [], weights':[], 'predictions':{}}
+    y_val: validation ground-truth
+    """
+    preds = sum([np.array(ensemble['predictions'][model_idx]) * ensemble['weights'][model_idx] 
+                 for model_idx in ensemble['models']])
+    ensemble_loss = binary_crossentropy(y_true=y_val, y_pred=preds).numpy().mean()
     
     return ensemble_loss
 
-def ensemble_selection(models, validation_data, optimize_metric='loss', iterations=15, threshold=0.001):
+def ensemble_selection(models, validation_data, optimize_metric='loss', iterations=10, threshold=0.005):
     """
-    This function implements ensemble selection (with replacement) from a library of neural networks
-    Parameters:
-    models: <list> keras models
+    Ensemble selection (with replacement) from a library of neural networks
+    models: <list> of <Model> object
     validation_data: <tuple> (X_val, y_val)
-    optimize_metrics: <str> 'loss', 'acc'
+    optimize_metrics: <str> 'loss' or 'acc'
     iterations: <int> number of iterations for selection
     """
     
-    #from copy import deepcopy
-    #import numpy as np
-    #np.random.seed(42)
-    #from keras.metrics import binary_crossentropy
-    
     if optimize_metric not in ['loss', 'acc']:
-        print("optimize_metric needs to be 'loss' or 'acc'.")
+        print("invalid metric: optimize_metric needs to be 'loss' or 'acc'.")
         return
     
     X_val, y_val = validation_data
-    # model pool {model_idx: {'model': model, 'predictions': predictions} }
-    model_pool = {} 
-    # ensemble {'models': [model indexes], 'predictions':{model_idx:predictions} 'weights': {model_idx: weight}}
+    
+    # model_pool : {model_idx: {'model': model, 'predictions': predictions} }
+    model_pool = {}
+    # ensemble : {'models': [model indexes], 'predictions':{model_idx:predictions} 'weights': {model_idx: weight}}
     ensemble = {'models':[], 'predictions':{}, 'weights':{}} 
-    ensemble_loss = pow(2,10)
+    
+    ensemble_loss, ensemble_acc = None, None
     ensemble_losses = []
     
-    # initialize model library and get best model
-    best_idx, min_loss = 0, pow(2, 10)
-    for model_idx, model in enumerate(models):
+    # 1 - Initialize model library and get best model for specified metric (acc/loss) - predictions are cached
+    best_idx, min_loss, max_acc = None, pow(2, 10), 0
+    
+    for model_idx, model in enumerate(models): # index of model in <list> models, model <Model> object
         predictions = model.predict(X_val)
         model_pool[model_idx] = {'model': model, 'predictions': predictions}
+        loss, acc = model.evaluate(X_val, y_val, batch_size=64, verbose=0, use_multiprocessing=False)
         
-        loss, acc = model.evaluate(X_val, y_val)
         if optimize_metric == 'loss':
-            if binary_crossentropy(y_val, predictions).numpy().mean() < min_loss:
-                best_idx = model_idx
+            if loss < min_loss:
+                best_idx, min_loss = model_idx, loss
         else:
-            # TODO: implement for accuracy
-            pass
+            if acc > max_acc:
+                best_idx, max_acc = model_idx, acc
     
-    # initialize ensemble
+    # 2 - Initialize ensemble with Best Model
     ensemble['models'].append(best_idx)
-    ensemble['predictions'][best_idx]= model_pool[best_idx]['predictions']
-    ensemble = update_ensemble_weights(ensemble)
-    #print(ensemble)
-    if optimize_metric == 'loss':
-        ensemble_loss = binary_crossentropy(y_val, model_pool[best_idx]['predictions']).numpy().mean()
-    else:
-        #TODO: implement for accuracy
-        pass
+    ensemble['predictions'][best_idx] = model_pool[best_idx]['predictions']
+    ensemble = update_weights(ensemble)
     
-    # ensemble selections
-    best_idx = 0
+    if optimize_metric == 'loss':
+        ensemble_loss = min_loss
+    else:
+        ensemble_acc = max_acc
+    
+    # 3 - Begin Ensemble Selection - TO DO: improve efficiency of algo to linear time by ordered selection
+    best_idx = 0 # the only model in the ensemble for now
     for i in range(iterations):
         for model_idx, model in model_pool.items():
-            # try to add
-            ensemble_copy = deepcopy(ensemble)
-            ensemble_copy['models'].append(model_idx)
-            ensemble_copy['predictions'][model_idx]= model_pool[best_idx]['predictions']
-            ensemble_copy = update_ensemble_weights(ensemble_copy)
+            
+            ensemble['models'].append(model_idx)
+            ensemble['predictions'][model_idx]= model_pool[model_idx]['predictions']
+            ensemble = update_weights(ensemble)
 
             # add to ensemble if score improves
-            loss = compute_ensemble_loss(ensemble_copy, y_val)
-            #print(loss)
-            
-            #print(ensemble_loss)
+            loss = compute_loss(ensemble, y_val)
             if (ensemble_loss - loss)/ensemble_loss > threshold:
                 ensemble_loss = loss
-                ensemble = deepcopy(ensemble_copy)
-                ensemble_losses.append(loss)
+                
+            else:
+                ensemble['models'].pop()
+                if model_idx not in ensemble['models']:
+                    ensemble['predictions'].pop(model_idx)
+                ensemble = update_weights(ensemble)
             
-        print(f"Iteration {i+1:3}: Ensemble Loss = {ensemble_loss}")  
-        #print(ensemble)
-        #print()
-        
-    model_ensemble = {'models':[], 'weights':[]}
-    for model_idx in ensemble['models']:
-        model_ensemble['models'].append(model_pool[model_idx]['model'])
-        model_ensemble['weights'].append(ensemble['weights'][model_idx])
-        
+            ensemble_losses.append(loss)
+        print(f"Iteration {i+1:3}: Ensemble Loss = {ensemble_loss}")
+    
+    # 4 - Final Ensemble
+    model_ensemble = {'models':None, 'weights':None}
+    model_ensemble['models'] = [model_pool[model_idx]['model'] for model_idx in ensemble['models']]
+    model_ensemble['weights'] = [ensemble['weights'][model_idx] for model_idx in ensemble['models']]
+    
     return model_ensemble, ensemble_losses
 
-def ensemble_prediction(model_ensemble, X):
+def ensemble_prediction(ensemble, X):
     """ 
-    This function makes ensemble prediction 
-    model_ensemble: <dict> {'models': [], 'weights':[]}
-    X: feature vector for prediction
+    Make ensemble (final model) prediction 
+    ensemble: <dict> {'models': [], 'weights':[]}
+    X: <array> input feature vector for prediction
     """
-    predictions = 0
-    for model_idx in range(len(model_ensemble['models'])):
-        predictions += np.array(model_ensemble['models'][model_idx].predict(X)) * model_ensemble['weights'][model_idx]
-        
-    return predictions
+    s = len(ensemble['models'])
+    prediction =  sum([np.array(ensemble['models'][i].predict(X)) * ensemble['weights'][i] for i in range(s)])
+    
+    return prediction
